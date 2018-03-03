069f3a91c1d464b6afd34ce0c755c61e02267988
Rafael <rbbernabeu@gmail.com>
2018-03-03 11:54:01 -0300

    adding function
    the undoReTweet function is not ready yet.

--- twitter.js
+++ twitter.js
@@ -1,13 +1,25 @@
 function deleteAllTweets() {
-
+    
     var f = function() {
         try {
             window.scrollTo(0, document.body.scrollHeight);
             document.querySelector('.js-actionDelete').click();
             document.querySelector('.delete-action').click();
         } catch (err) {}
-    };
+    }
 
     setInterval(f, 2000);
+}
 
+
+function undoReTweet() { //TODO
+    
+    var f = function() {
+        try {
+            $('.tweet .ProfileTweet-actionButtonUndo').css('display','inline-block').click();
+            window.scrollTo(0,document.body.scrollHeight);
+        } catch (err) { console.log(err) }
+    }
+    
+    setInterval(f, 5000);
 }
