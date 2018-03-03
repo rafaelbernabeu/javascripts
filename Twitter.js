39bf420c7c9eeadf86b7813c9f01a31cc8f55516
Rafael <rbbernabeu@gmail.com>
2018-03-03 02:04:53 -0300

    Create Twitter.js

---
+++ Twitter.js
@@ -0,0 +1,13 @@
+function apagaTodosTweets() {
+
+    var f = function() {
+        try {
+            window.scrollTo(0, document.body.scrollHeight);
+            document.querySelector('.js-actionDelete').click();
+            document.querySelector('.delete-action').click();
+        } catch (err) {}
+    };
+
+    setInterval(f, 2000);
+
+}
